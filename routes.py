26eb1e12e2c5e2eab72f797e2459330c39254bde
Jeremy <junweima2@gmail.com>
2021-11-29 18:28:27 -0500

    init

--- routes.py
+++ routes.py
@@ -1,19 +1,18 @@
-from flask import Flask, render_template
+import pandas as pd
+import os
+from flask import Flask, render_template, request, g
 app = Flask(__name__)
+parent_folder = '/media/jeremy/c602a316-f553-419a-859e-688208757bde/MSRVTT'
+df = pd.read_csv(os.path.join(parent_folder, 'MSRVTT_JSFUSION_test.csv'))
 
-# two decorators, same function
-@app.route('/')
-@app.route('/index.html')
-def index():
-    return render_template('index.html', the_title='Tiger Home Page')
 
-@app.route('/symbol.html')
-def symbol():
-    return render_template('symbol.html', the_title='Tiger As Symbol')
+@app.route('/', methods=['POST'])
+def handle():
+    if request.method == 'POST':
+        if request.form.get('generate') == 'generate':
+            vid = os.path.join(parent_folder, 'videos/all', df.iloc[0].video_id + '.mp4')
+            return render_template('index.html', vid=vid)
 
-@app.route('/myth.html')
-def myth():
-    return render_template('myth.html', the_title='Tiger in Myth and Legend')
 
 if __name__ == '__main__':
     app.run(debug=True)
